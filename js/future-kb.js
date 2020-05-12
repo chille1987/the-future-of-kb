9a22c4cd08b4a3a8729f4c8bf7d910b2e389c45b
Denis Omerovic <denis.omerovic24@gmail.com>
2020-05-12 19:07:00 +0200

    Finished admin toolbar

--- js/future-kb.js
+++ js/future-kb.js
@@ -1,3 +1,25 @@
 $(function() {
+  // Admin toolbar
+  const openDropdown = document.querySelector('.open-dropdown')
+  const userDropdown = document.querySelector('.user-dropdown')
+
+  openDropdown.addEventListener('click', () => {
+    userDropdown.classList.toggle('active');
+  });
+
+  document.addEventListener("click", (e) => {
+    let targetElement = e.target;
+
+    do {
+      if (targetElement == userDropdown || targetElement == openDropdown) {
+        return;
+      }
+      targetElement = targetElement.parentNode;
+    } while (targetElement);
+
+    userDropdown.classList.remove('active');
+  });
+
+
   $('#categories').tabs();
 })
